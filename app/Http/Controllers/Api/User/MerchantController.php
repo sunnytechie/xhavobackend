8e154546470990da73fd25dfe03dcb791c2d5af1
sunnytechie <sunnyict001@gmail.com>
2023-10-09 16:39:59 +0100

    category added to merchant

--- app/Http/Controllers/Api/User/MerchantController.php
+++ app/Http/Controllers/Api/User/MerchantController.php
@@ -5,23 +5,33 @@ namespace App\Http\Controllers\Api\User;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class MerchantController extends Controller
 {
     //list of users with user_type = merchants
     public function index()
     {
-        //
-
         $merchants = User::where('user_type', 'merchant')
                         ->with('merchant')
                         ->with('reviews')
                         ->get();
 
+                        $merchantData = array();
+                        foreach ($merchants as $merchant) {
+                            //category
+                            $category = Category::where('id', $merchant->merchant->category_id)->first();
+
+                            $merchantData[] = array(
+                                'category' => $category->title,
+                                'merchant' => $merchant,
+                            );
+                        }
+
         return response()->json([
             'success' => true,
             'message' => 'List of all merchants',
-            'data' => $merchants
+            'data' => $merchantData
         ]);
     }
 }
